66137a00574d4c7615dd63f4b6deeee0503bb5b1
AzfFoysal <abuzehadfoysal@gmail.com>
2023-05-03 10:13:12 +0600

    Menu & Exit Added

--- app/src/main/java/com/example/kids_learning_app/MainActivity.java
+++ app/src/main/java/com/example/kids_learning_app/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.kids_learning_app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -23,5 +28,37 @@ public class MainActivity extends AppCompatActivity {
         TabLayoutMediator tm = new TabLayoutMediator(tl, pager, true,
                 (tab, position) -> tab.setText(tabTitle[position]));
         tm.attach();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.exit){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Do You Want TO Exit?");
+            builder.setCancelable(true);
+
+            builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int i) {
+                    finish();
+                }
+            });
+           builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialog, int i) {
+                   dialog.cancel();
+               }
+           });
+           AlertDialog alertDialog = builder.create();
+           alertDialog.show();
+        }
+        return true;
     }
 }
